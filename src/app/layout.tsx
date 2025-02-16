eae5f6e202450dcbb968ae6ec1ff537c2f7b4413
chiefkarim <krimotop2016@gmail.com>
2025-02-16 17:31:29 +0100

    feat: remove old website files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Karim Menna ",
+  title: "Karim Menna",
   description:
-    "As a software engineer at Big-mama, I specialize in crafting robust solutions using Docker, Next.js, and Strapi... with a passion for learning new technologies. Additionally, I offer freelance services in Shopify and Webflow development. I love building websites and apps for global businesses and individuals and seeing them rise.",
+    "As a software engineer , I specialize in crafting robust solutions using Docker, Next.js, and Strapi... with a passion for learning new technologies. Additionally, I offer freelance services in Shopify and Webflow development. I love building websites and apps for global businesses and individuals and seeing them rise.",
 };
 
 export default function RootLayout({
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="!scroll-smooth" data-wf-domain="karimmenna.com">
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
